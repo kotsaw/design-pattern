bf5bbd09e4bde03fb938b228858408cd893da8d7
BARRY Guillaume <guillaume.barry@soprabanking.com>
2020-11-30 16:18:30 +0100

    Ajout jambon

--- src/test/java/com/tetras/PizzaTest.java
+++ src/test/java/com/tetras/PizzaTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PizzaTest {
     @Test
     void testCreatePizza() {
-        // Reine : champignons, emmental, sauce tomate
+        // Reine : champignons, emmental, sauce tomate, jambon
         Pizza maPizza = new Pizza();
 
         assertFalse(maPizza.isAnchois());
